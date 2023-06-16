642bbb186e6622e5dd48eb39b754d0dbc925678a
Carlos Luján <cdlujanp@gmail.com>
2023-06-16 01:48:13 -0500

    apps fields added n new app persons

--- personal_finance/settings.py
+++ personal_finance/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'phonenumber_field',
+    'django_address',
+    'personal_finance.apps.persons',
 ]
 
 MIDDLEWARE = [
@@ -127,3 +131,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# phone number field
+PHONENUMBER_DEFAULT_REGION = 'CO'
+PHONENUMBER_DB_FORMAT = 'NATIONAL'
