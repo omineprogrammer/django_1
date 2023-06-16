673cb59331548bd1c725b78df401636d6614a474
Carlos Luján <cdlujanp@gmail.com>
2023-06-15 20:58:18 -0500

    config database in app settings

--- personal_finance/settings.py
+++ personal_finance/settings.py
@@ -74,9 +74,15 @@ WSGI_APPLICATION = 'personal_finance.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "django_1",
+        "USER": "root",
+        "HOST": "database-container",
+        "PORT": "5432",
+        "OPTIONS": {
+            "passfile": ".pg_pass",
+        },
     }
 }
 
